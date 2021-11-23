9e5faeba9121c0773bb53d13fa3c423babf1b0df
KeithKelleher <keith.kelleher@ncats.nih.gov>
2021-11-23 01:06:31 -0500

    scale tooltip location appropriately

--- src/protvista-uniprot.ts
+++ src/protvista-uniprot.ts
@@ -608,7 +608,10 @@ class ProtvistaUniprot extends LitElement {
         console.log(prop + ': ' + manBounds[prop]);
       }
       const [x, y] = e.detail.coords;
-      tooltip.x = x;
+
+      const distX = window.pageYOffset + manBounds.getBoundingClientRect().top;
+
+      tooltip.x = x - distX;
       tooltip.y = y;
     }
   }
